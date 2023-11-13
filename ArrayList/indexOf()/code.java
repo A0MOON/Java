a23189dbfd8ac301c4e80080bed02e3195cb1625
A0MOON <149945555+A0MOON@users.noreply.github.com>
2023-11-13 22:59:35 +0900

    Create code.java

---
+++ ArrayList/indexOf()/code.java
@@ -0,0 +1,16 @@
+public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
